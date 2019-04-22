2fca0d745bb67080ae7f75797ad7aa8d07c473d8
Roberto Valencia Murillo <vamyur@hotmail.com>
2019-04-22 15:42:13 -0600

    Added Comic Books List view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -70,5 +70,10 @@ namespace ComicBookGallery.Data
             }
             return comicBookToReturn;
         }
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
     }
 }
